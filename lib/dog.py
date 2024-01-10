c465e24e4790acb94c5601abfc4b63e67096ee10
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-10 15:08:48 -0500

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        print(f"{name} is born!")
+        self.name = name
+        self.breed = breed
+
+    # def bark(self):
+    #     print("Woof!")
+
+    # def adopt(dog, owner_name):
+    #     dog.owner = owner_name
+
+    # def get_adopted(self, owner_name):
+    #     self.owner = owner_name
+
+    
